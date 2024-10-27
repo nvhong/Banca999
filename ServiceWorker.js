775dfa74a33ba6cd73bbb007df9d21fda8f6b2b8
nvhong <48241485+nvhong@users.noreply.github.com>
2024-10-27 10:11:55 +0700

    u

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Banca999WebGL5-1.0";
+const contentToCache = [
+    "Build/WebManualBuild.loader.js",
+    "Build/WebManualBuild.framework.js",
+    "Build/WebManualBuild.data",
+    "Build/WebManualBuild.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
